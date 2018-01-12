cda83c1d48fe3383605a18cb6c560fee7a5622ee
sabas1080 <s@theinventorhouse.org>
2018-01-12 15:01:55 -0600

    removed unnecessary code

--- enums0.d.ts
+++ enums0.d.ts
@@ -1,15 +1,1 @@
-// Auto-generated. Do not edit.
-
-
-    /**
-     * Location of temperature
-     */
-
-    declare enum TemperatureLocation {
-    Object = 0,
-    Ambiant = 1,
-    }
-declare namespace bmp085 {
-}
-
-// Auto-generated. Do not edit. Really.
+// Will be auto-generated if needed.
--- shims.d.ts
+++ shims.d.ts
@@ -1,17 +1,1 @@
-// Auto-generated. Do not edit.
-
-
-    /**
-     * Functions to support the BMP085sensor
-     */
-    //%
-declare namespace bmp085 {
-
-    /**
-     * Reads the temperature from the IR sensor MLX90614
-     */
-    //% blockId=bmp085 block="temperature %loc" shim=mlx90614::temperature
-    function temperature(loc: TemperatureLocation): number;
-}
-
-// Auto-generated. Do not edit. Really.
+// Will be auto-generated if needed.
