b838c74ba8eab7856d28f7fb7b0e91c82bae2c69
sabas1080 <s@theinventorhouse.org>
2018-01-11 23:00:52 -0600

    Commit Initial

---
+++ shims.d.ts
@@ -0,0 +1,17 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Functions to support the BMP085sensor
+     */
+    //%
+declare namespace bmp085 {
+
+    /**
+     * Reads the temperature from the IR sensor MLX90614
+     */
+    //% blockId=bmp085 block="temperature %loc" shim=mlx90614::temperature
+    function temperature(loc: TemperatureLocation): number;
+}
+
+// Auto-generated. Do not edit. Really.
