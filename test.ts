f26e9df073af8925d509036cdf1793ebe44d5e2a
MarcusBCN <63583270+MarcusBCN@users.noreply.github.com>
2020-04-21 19:20:14 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+input.onButtonPressed(Button.B, function () {
+    bmp085.debug(true_table.On)
+})
+input.onButtonPressed(Button.A, function () {
+    bmp085.debug(true_table.Off)
+})
+serial.redirectToUSB()
+bmp085.beginBMP085(bmp_bmpmode.ULTRALOWPOWER)
+bmp085.debug(true_table.Off)
+basic.forever(function () {
+    serial.writeString("temp:")
+    serial.writeNumber(bmp085.readTemperature())
+    serial.writeLine("")
+    serial.writeString("pres:")
+    serial.writeNumber(bmp085.readPressure() / 100)
+    serial.writeLine("hPa")
+})
